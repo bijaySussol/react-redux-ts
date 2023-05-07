e1721923e16020adfcf53a7479dfc765e0d6142f
bijaySussol <bijay@sussol.net>
2023-05-07 12:49:22 +0545

    logic

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,39 +1,16 @@
 import React from "react";
-import ReactDOM  from "react-dom";
-
-interface AppProps {
-    color? : string;
-}
-interface AppState {
-    counter: number;
-}
-
-class App extends React.Component<AppProps, AppState> {
-    
-    constructor(props: AppProps) {
-        super(props);
-        this.state = { counter: 0 };
-    }
-
-    onIncrement = () : void => {
-        this.setState({ counter: this.state.counter + 1 });
-    };
-    onDecrement = () : void => {
-        this.setState({ counter: this.state.counter -1})
-    }
-
-    render () {
-        return (
-            <div>
-                <button onClick={this.onIncrement}>Increment</button>
-                <button onClick={this.onDecrement}>Decrement</button>
-                {this.state.counter}
-            </div>
-        );
-    }
-}
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { App } from "./components/app";
+import { reducers } from "./reducers";
+//https://jsonplaceholder.typicode.com/todos
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <App />, 
-    document.querySelector('#root')
-)
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
